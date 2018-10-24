9b4cdb34a0c6a6a1775d35d8aab82f6ae00c0e54
James McKenzie <jamesmck1987@gmail.com>
2018-10-24 09:32:56 +0100

    better solution for knockout background

--- src/components/FullScreenBorder.js
+++ src/components/FullScreenBorder.js
@@ -6,21 +6,28 @@ const borderWidth = 20;
 const FullScreenBorder = ({ children }) => (
   <div
     className={css`
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      height: calc(100vh - ${borderWidth * 2}px);
-      width: calc(100vw - ${borderWidth * 2}px);
-
-      border: 20px solid green;
-      // border-image-source: linear-gradient(#3ec8ac, #4e90a4);
-
-      border-image-source: url("https://images.unsplash.com/photo-1513635269975-59663e0ac1ad?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=18dcdd4e1c2627bc3c9bf68645c9ae92&auto=format&fit=crop&w=2550&q=80");
-      border-image-slice: 20;
+      height: calc(100vh);
+      width: calc(100vw);
+      background: url("https://images.unsplash.com/photo-1513635269975-59663e0ac1ad?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=18dcdd4e1c2627bc3c9bf68645c9ae92&auto=format&fit=crop&w=2550&q=80")
+        center;
       border-width: ${borderWidth}px;
     `}
   >
-    {children}
+    <div
+      className={css`
+        position: absolute;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        top: ${borderWidth}px;
+        left: ${borderWidth}px;
+        background-color: white;
+        height: calc(100vh - ${borderWidth * 2}px);
+        width: calc(100vw - ${borderWidth * 2}px);
+      `}
+    >
+      {children}
+    </div>
   </div>
 );
 
--- src/components/KnockoutText.js
+++ src/components/KnockoutText.js
@@ -9,13 +9,6 @@ const KnockoutText = ({ children }) => (
     className={css`
       background: url("https://images.unsplash.com/photo-1513635269975-59663e0ac1ad?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=18dcdd4e1c2627bc3c9bf68645c9ae92&auto=format&fit=crop&w=2550&q=80")
         center;
-
-      // background: #74ebd5;
-      // background: -webkit-linear-gradient(left, #3ec8ac 0%, #4e90a4 100%),
-      //   -webkit-linear-gradient(left, #3ec8ac 0%, #4e90a4 100%);
-      // background: linear-gradient(to right, #3ec8ac 0%, #4e90a4 100%),
-      //   linear-gradient(to right, #3ec8ac 0%, #4e90a4 100%);
-      color: red;
       -webkit-text-fill-color: transparent;
       -webkit-background-clip: text;
 
@@ -26,8 +19,6 @@ const KnockoutText = ({ children }) => (
       ${media.phone`font-size: 5rem`};
       text-transform: lowercase;
 
-      // text-align: center;
-      // margin: 0 20px !important;
       padding: 15px 8px;
     `}
   >
