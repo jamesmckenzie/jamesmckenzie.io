2eb168550c65d43a99ff3df38383b7e40a9f2d4f
James McKenzie <jamesmck1987@gmail.com>
2019-02-19 21:52:42 +0000

    tracking links

--- src/components/SocialBar/SocialBar.tsx
+++ src/components/SocialBar/SocialBar.tsx
@@ -2,23 +2,18 @@ import React from "react";
 import { Box, Flex, Link } from "rebass";
 import styled from "styled-components";
 import { IoLogoGithub, IoLogoTwitter, IoLogoLinkedin } from "react-icons/io";
+import { OutboundLink } from "gatsby-plugin-google-analytics";
 
 const Absolute = styled(Flex)`
   position: absolute;
   right: 20px;
-  /* top: 60px; */
   height: 100vh;
   z-index: 1;
 `;
 
-const SocialLink = styled(Link)`
+const SocialLink = styled(OutboundLink)`
   color: white;
 `;
-
-SocialLink.defaultProps = {
-  mb: 3,
-  as: "a"
-};
 
 const SocialBar = () => (
   <Absolute flexDirection="row" justifyContent="center" mt={3}>
