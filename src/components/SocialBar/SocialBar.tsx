3694ab58d7870589df66832cc06f881f601ec85f
James McKenzie <jamesmck1987@gmail.com>
2019-02-20 21:50:21 +0000

    fix link

--- src/components/SocialBar/SocialBar.tsx
+++ src/components/SocialBar/SocialBar.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Box, Flex, Link } from "rebass";
 import styled from "styled-components";
 import { IoLogoGithub, IoLogoTwitter, IoLogoLinkedin } from "react-icons/io";
-import { OutboundLink } from "gatsby-plugin-google-analytics";
 
 const Absolute = styled(Flex)`
   position: absolute;
@@ -11,7 +10,7 @@ const Absolute = styled(Flex)`
   z-index: 1;
 `;
 
-const SocialLink = styled(OutboundLink)`
+const SocialLink = styled(Link)`
   color: white;
 `;
 
